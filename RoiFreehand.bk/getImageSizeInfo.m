d38982fa2be7c7dc7d9d3c56797fad2df3e273ad
hubo <b.hu@stud.uni-heidelberg.de>
2021-03-04 15:50:56 +0100

    Deleted RoiFreehand.bk

--- RoiFreehand.bk/getImageSizeInfo.m
+++
@@ -1,7 +0,0 @@
-function imageInfo = getImageSizeInfo(hImg)
-[xdata,ydata,cdata] = getimage(hImg);
-imageSize = size(cdata);
-imageInfo.xdata = xdata;
-imageInfo.ydata = ydata;
-imageInfo.imageSize = imageSize;
-
--- RoiFreehand.bk/isaRoiPatch.m
+++
@@ -1,8 +0,0 @@
-function result = isaRoiPatch(hobj)
-result = false;
-if ishandle(hobj) && isvalid(hobj) && isprop(hobj,'Tag')
-    tag = get(hobj,'Tag');
-    if strfind(tag,'roi_')
-        result = true;
-    end
-end
