ddaf355682819514df58b296edd367f525055561
Bo Hu <19688778+aloejhb@users.noreply.github.com>
2022-10-03 15:15:20 +0200

    Add tests for BUnwarpJ (wip2)

--- tests/createTestExperiment.m
+++ tests/createTestExperiment.m
@@ -1,17 +1,38 @@
-function expStruct = createTestExperiment()
-trialStructList = {}
-trialStructList{1} = createTestMovie()
-trialStructList{2}= createTestMovie('ampList', [2, 2, 3]);
-A = [2 0 0; 0.33 1 0; 0 0 1];
+function expStruct = createTestExperiment(tmpExpDir)
+    if ~exist(tmpExpDir, 'dir')
+        error('Temporary experiment directory does not exist!')
+    end
+    trialStructList = {};
+    trialStructList{1} = createTestMovie();
+    affineMat2 = [1 0 0; 0 1 0; -3 2 1];
+    trialStructList{2}= createTestMovie('ampList', [2, 2, 3], 'affineMat', affineMat2);
+    affineMat3 = [1.2 0 0; 0.33 1 0; 2 3 1];
+    trialStructList{3}= createTestMovie('ampList', [2, 2, 3], 'affineMat', affineMat3);
+    nTrial = length(trialStructList);
 
-% transform one of the movies
+    % Save trial movie
+    for k=1:nTrial
+        saveMovie(trialStructList{k}.rawMovie,...
+                  fullfile(tmpExpDir, sprintf('trial%02d.tif', k)));
+    end
 
-testStruct.trial = TrialModel('mockMovie', mockMovie);
+    rawFileList = arrayfun(@(x)(sprintf('trial%02d', x)), 1:nTrial, 'UniformOutput', false);
+    resultDir = fullfile(tmpExpDir, 'result');
+    expInfo.name = 'test-experiment';
+    expInfo.frameRate = 5;
+    expInfo.nPlane = 1;
+    expInfo.mapSize = [20, 24];
 
-% Save trial movie
-% Initiate experiment
 
-expStruct.myexp = myexp;
-expStruct.trialStructList = trialStructList
+    % Initiate experiment
+    myexp = NrModel('rawDataDir', tmpExpDir,...
+                    'rawFileList', rawFileList,...
+                    'resultDir',resultDir,...
+                    'expInfo',expInfo);
+    expStruct.myexp = myexp;
+    expStruct.trialStructList = trialStructList;
+end
 
+function saveMovie(rawMovie, filePath)
+    movieFunc.saveTiff(uint8(rawMovie), filePath);
 end
--- tests/createTestMovie.m
+++ tests/createTestMovie.m
@@ -8,15 +8,15 @@ addParameter(pa, 'affineMat', [1, 0, 0; 0, 1, 0; 0, 0, 1], @ismatrix);
 parse(pa,varargin{:})
 pr = pa.Results;
 
-movieSize = [12, 10, 20];
+movieSize = [20, 24, 22];
 mockMovie.name = 'mock_movie';
 mockMovie.meta = struct('width', movieSize(1),...
                         'height', movieSize(2),...
                         'totalNFrame', movieSize(3));
 rawMovie = zeros(movieSize);
-roiList = {[3,3;3,4;3,5;4,3;4,4;4,5;5,3;5,4;5,5]
-           [5,8;5,9;6,8;6,9;7,8;7,9;6,7],
-           [9,6;9,7;10,6;10,7;11,6]
+roiList = {[3,3;3,4;3,5;4,3;4,4;4,5;5,3;5,4;5,5] + 4
+           [5,8;5,9;6,8;6,9;7,8;7,9;6,7] + 4,
+           [9,6;9,7;10,6;10,7;11,6] + 4
           };
 
 timeTraceMat = zeros(length(roiList), movieSize(3));
@@ -33,16 +33,27 @@ for k=1:length(roiList)
     timeTrace = computeTimeTrace(start, dur, base, amp, movieSize(3));
     timeTraceMat(k, :) = timeTrace;
 end
-rawMovie = shiftMovieYx(rawMovie, offsetYx)
+rawMovie = warpMovie(rawMovie, pr.affineMat);
 movieStruct.rawMovie = rawMovie;
 movieStruct.anatomy = mean(rawMovie, 3);
 movieStruct.timeTraceMat = timeTraceMat;
 end
 
 
+function timeTrace = computeTimeTrace(start, dur, base, amp, totalT)
+    timeTrace =  base * ones(1, totalT);
+    timeTrace(start:start+dur-1) = amp;
+end
+
+function dynamic = computeDynamic(mask, start, dur, base, amp, movieSize)
+    dynamic = zeros(movieSize);
+    dynamic = base * repmat(mask, 1, 1, movieSize(3));
+    dynamic(:, :, start:start+dur-1) = amp * repmat(mask, 1, 1, dur);
+end
+
 function warped = warpMovie(rawMovie, affineMat)
-    tform = affinetform2d(affineMat)
-    warped = imwarp(rawMovie,tform);
+    tform = affine2d(affineMat);
+    warped = movieFunc.imwarpSame(rawMovie,tform);
 end
 
 
