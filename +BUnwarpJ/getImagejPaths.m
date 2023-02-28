02c359a5966c02d74505a3b73259d954a902d101
Bo Hu <19688778+aloejhb@users.noreply.github.com>
2023-02-28 15:49:19 +0100

    Check config file exists

--- +BUnwarpJ/getImagejPaths.m
+++ +BUnwarpJ/getImagejPaths.m
@@ -2,9 +2,14 @@ function imagejPaths = getImagejPaths()
     currPath = mfilename('fullpath');
     [currDir, ~, ~] = fileparts(currPath);
     neuRoiDir = fullfile(currDir, '..');
-
+    configFile = fullfile(neuRoiDir, 'config.ini');
+    
+    if ~exist(configFile, 'file')
+        error(sprintf('Configuration file %s does not exists!', configFile))
+    end
+    
     ini = helper.IniConfig();
-    ini.ReadFile(fullfile(neuRoiDir, 'config.ini'));
+    ini.ReadFile();
 
     imagejPaths = ini.GetValues('[bunwarpj]');
 end
