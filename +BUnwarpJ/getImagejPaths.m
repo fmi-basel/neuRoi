7429cc2943727ae6d4bcd6aabf8408a2cb3f1902
Bo Hu <bo.hu@fmi.ch>
2023-02-28 18:22:49 +0100

    Automatic detection of imagej paths #13

--- +BUnwarpJ/getImagejPaths.m
+++ +BUnwarpJ/getImagejPaths.m
@@ -9,8 +9,37 @@ function imagejPaths = getImagejPaths()
     end
     
     ini = helper.IniConfig();
-    ini.ReadFile();
+    ini.ReadFile(configFile);
 
-    imagejPaths = ini.GetValues('[bunwarpj]');
+    ijPath = ini.GetValues('[bunwarpj]', 'path');
+
+    imagejPaths = {};
+
+    imagejPaths{1} = dir(fullfile(ijPath, 'plugins', 'bUnwarpJ_-*.jar'));
+
+    if isempty(imagejPaths{1})
+        error('bUnwarpJ not found in imagej.')
+    end
+
+    imagejPaths{2} = dir(fullfile(ijPath, 'plugins', 'mpicbg_*.jar'));
+
+    if isempty(imagejPaths{2})
+        error('mpicbg plugin not found in imagej.')
+    end
+
+    imagejPaths{3} = dir(fullfile(ijPath, 'jars', 'ij-*.jar'));
+
+    if isempty(imagejPaths{3})
+        error('ij not found in imagej.')
+    end
+
+    imagejPaths{4} = dir(fullfile(ijPath, 'jars', 'mpicbg-*.jar'));
+
+    if isempty(imagejPaths{4})
+        error('mpicbg jar not found in imagej.')
+    end
+
+    imagejPaths = cellfun(@(x) fullfile(x.folder, x.name),...
+        imagejPaths, 'UniformOutput', false);
 end
 
